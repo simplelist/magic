3e1e2bec236b8be4b29aa4c46d8917e86005fb9b
shijikun <shijikun@91zdan.com>
2018-03-24 00:07:16 +0800

    支持配置多个插件,反射使用 ibatis 自带反射工具

--- src/main/java/plugins/JPA.java
+++ src/main/java/plugins/JPA.java
@@ -1,15 +1,16 @@
 package plugins;
 
 import org.apache.ibatis.executor.statement.PreparedStatementHandler;
+import org.apache.ibatis.executor.statement.RoutingStatementHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
-import org.springframework.util.ReflectionUtils;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Properties;
@@ -20,28 +21,38 @@ public class JPA implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        StatementHandler handler = (StatementHandler) invocation.getTarget();
+        MappedStatement statement;
+        PreparedStatementHandler preparedStatementHandler;
+        BoundSql boundSql;
+        RoutingStatementHandler target = (RoutingStatementHandler) invocation.getTarget();
 
-        PreparedStatementHandler delegate = (PreparedStatementHandler) getFieldValue(handler, "delegate");
-        MappedStatement mappedStatement = (MappedStatement) getFieldValue(delegate, "mappedStatement");
-        if (SqlCommandType.SELECT.equals(mappedStatement.getSqlCommandType())) {
-            DefaultParameterHandler parameterHandler = (DefaultParameterHandler) getFieldValue(delegate, "parameterHandler");
-            BoundSql boundSql = delegate.getBoundSql();
-            setSQL(boundSql, mappedStatement, parameterHandler);
+        //多个插件
+        if (Proxy.isProxyClass(target.getClass())) {
+            Plugin h = (Plugin) SystemMetaObject.forObject(target).getValue("h");
+            RoutingStatementHandler handler = (RoutingStatementHandler) SystemMetaObject.forObject(h).getValue("target");
+            preparedStatementHandler = (PreparedStatementHandler) SystemMetaObject.forObject(handler).getValue("delegate");
+        } else {
+            //单个插件
+            preparedStatementHandler = (PreparedStatementHandler) SystemMetaObject.forObject(target).getValue("delegate");
+        }
+        statement = (MappedStatement) SystemMetaObject.forObject(preparedStatementHandler).getValue("mappedStatement");
+        boundSql = (BoundSql) SystemMetaObject.forObject(preparedStatementHandler).getValue("boundSql");
+
+        if (SqlCommandType.SELECT.equals(statement.getSqlCommandType())) {
+
+            setSQL(boundSql, statement, (DefaultParameterHandler) SystemMetaObject.forObject(preparedStatementHandler).getValue("parameterHandler"));
         }
         return invocation.proceed();
     }
 
     private void setSQL(BoundSql boundSql, MappedStatement mappedStatement, DefaultParameterHandler parameterHandler) throws NoSuchFieldException {
-        Field field = ReflectionUtils.findField(boundSql.getClass(), "sql");
-        field.setAccessible(true);
         Class<?> resultEntity = mappedStatement.getResultMaps().get(0).getType();
         String tableName = resultEntity.getSimpleName().toLowerCase();
-        String statementId = mappedStatement.getId();//selectByField
+        String statementId = mappedStatement.getId();//selectById
         String boundSqlStr = boundSql.getSql();
         if (boundSqlStr == null || boundSqlStr.equals("")) {
             StringBuffer sql = new StringBuffer("select * from ");
-            //todo 驼峰下划线处理 
+            //todo 驼峰下划线处理
             sql.append(tableName);
             if (statementId.contains("By")) {
                 String methodName = statementId.substring(statementId.lastIndexOf("By") + 2).toLowerCase();
@@ -54,37 +65,31 @@ public class JPA implements Interceptor {
                     sql.append(parameterObject);
                     sql.append("%'");
                 } else {//精确查询 =
-                    //如果包含 and 表示多个查询条件
                     if (methodName.contains("and")) {
+                        //如果包含 and 表示多个查询条件
                         String[] fields = methodName.split("and");
-                        HashMap paramMap = (HashMap) parameterObject;//将参数强转为 HashMap
+                        HashMap paramMap = (HashMap) parameterObject;
                         boolean first = true;
                         for (int i = 0; i < fields.length; i++) {
-                            //拼接 and 语句
                             if (!first) {
                                 sql.append(" and ");
                             }
                             processEquals(paramMap.get(i + ""), sql, fields[i]);
                             first = false;
+
                         }
                     } else {
-                        //处理一个查询条件
                         processEquals(parameterObject, sql, methodName);
                     }
                 }
 
             }
-            ReflectionUtils.setField(field, boundSql, sql.toString());
+            SystemMetaObject.forObject(boundSql).setValue("sql", sql.toString());
         }
+
+
     }
 
-    /**
-     * 处理准确查询,拼接 SQL 语句
-     *
-     * @param value
-     * @param sql
-     * @param methodName
-     */
     private void processEquals(Object value, StringBuffer sql, String methodName) {
         sql.append(methodName);
         sql.append(" = '");
@@ -92,11 +97,6 @@ public class JPA implements Interceptor {
         sql.append("'");
     }
 
-    private Object getFieldValue(StatementHandler handler, String fieldName) {
-        Field field = ReflectionUtils.findField(handler.getClass(), fieldName);
-        field.setAccessible(true);
-        return ReflectionUtils.getField(field, handler);
-    }
 
     @Override
     public Object plugin(Object target) {
